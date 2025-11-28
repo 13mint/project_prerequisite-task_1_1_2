e906557083421c10507db0da1d05b6e9b66126f7
13mint <13mint07pineapple13@gmail.com>
2025-11-28 14:17:34 +0900

    fixed problem with connetcion

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -8,9 +8,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
 
-    }
+    private final Connection connection = Util.getConnection();
 
     public void createUsersTable() {
         String sql = "CREATE TABLE IF NOT EXISTS users (" +
@@ -20,8 +19,7 @@ public class UserDaoJDBCImpl implements UserDao {
                 "age TINYINT NOT NULL" +
                 ")";
 
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement()) {
+        try (Statement statement = connection.createStatement()) {
 
             statement.executeUpdate(sql);
             System.out.println("Таблица users создана!");
@@ -33,8 +31,7 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void dropUsersTable() {
         String sql = "DROP TABLE IF EXISTS users";
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement()) {
+        try (Statement statement = connection.createStatement()) {
 
             statement.executeUpdate(sql);
             System.out.println("Таблица users удалена!");
@@ -46,26 +43,30 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void saveUser(String name, String lastName, byte age) {
         String sql = "INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)";
-        try(Connection connection = Util.getConnection();
-        ) {
-            try(PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            ) {
-                preparedStatement.setString(1, name);
-                preparedStatement.setString(2, lastName);
-                preparedStatement.setByte(3, age);
 
-                preparedStatement.executeUpdate();
+            try(PreparedStatement ps = connection.prepareStatement(sql)) {
+                ps.setString(1, name);
+                ps.setString(2, lastName);
+                ps.setByte(3, age);
+
+                ps.executeUpdate();
 
                 System.out.println("Пользователь с именем - " + name + " добавлен в базу данных");
 
+            }catch (SQLException e) {
+                throw new RuntimeException(e);
             }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     public void removeUserById(long id) {
-
+        String sql = "DELETE FROM users WHERE id = ?";
+        try(PreparedStatement ps = connection.prepareStatement(sql)){
+            ps.setLong(1, id);
+            ps.executeUpdate();
+            System.out.println("Пользователь с id " + id + " удалён.");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<User> getAllUsers() {
@@ -73,15 +74,14 @@ public class UserDaoJDBCImpl implements UserDao {
 
         String sql = "SELECT * FROM users";
 
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement();
+        try (Statement statement = connection.createStatement();
              ResultSet rs = statement.executeQuery(sql)) {
 
             while (rs.next()) {
                 User user = new User(
                         rs.getLong("id"),
                         rs.getString("name"),
-                        rs.getString("last_name"),
+                        rs.getString("lastName"),
                         rs.getByte("age")
                 );
                 users.add(user);
@@ -95,6 +95,11 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-
+        String sql = "TRUNCATE TABLE users";
+        try(Statement statement = connection.createStatement()){
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,20 +3,23 @@ package jm.task.core.jdbc.util;
 import java.sql.*;
 
 public class Util {
-    public static Connection getConnection() throws SQLException {
+    public static Connection getConnection() {
         String url = "jdbc:mysql://localhost:3306/users_db";
         String user = "root";
         String pass = "Purple8star#";
+        try {
+            Connection conn = DriverManager.getConnection(url, user, pass);
 
-        Connection conn = DriverManager.getConnection(url, user, pass);
+            try (Statement stmt = conn.createStatement();
+                 ResultSet rs = stmt.executeQuery("SELECT NOW()")) {
 
-        try (Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery("SELECT NOW()")) {
-
-            if (rs.next()) {
-                System.out.println("🕒 Текущее время в БД: " + rs.getString(1));
+                if (rs.next()) {
+                    System.out.println("🕒 Текущее время в БД: " + rs.getString(1));
+                }
             }
+            return conn;
+        } catch (SQLException e) {
+            throw new RuntimeException("Ошибка подключения к БД", e);
         }
-        return conn;
     }
 }
