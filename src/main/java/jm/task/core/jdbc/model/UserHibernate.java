37f3297e667d0a1f824efd389eba684d7f8f1628
13mint <13mint07pineapple13@gmail.com>
2025-12-02 13:21:44 +0900

    created hibernateUtil, hibernateImpl

---
+++ src/main/java/jm/task/core/jdbc/model/UserHibernate.java
@@ -0,0 +1,55 @@
+package jm.task.core.jdbc.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class UserHibernate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String lastName;
+    private byte age;
+
+    public UserHibernate() {}
+
+    public UserHibernate(String name, String lastName, byte age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Byte getAge() {
+        return age;
+    }
+
+    public void setAge(Byte age) {
+        this.age = age;
+    }
+}
---
+++ src/main/java/jm/task/core/jdbc/util/HibernateUtil.java
@@ -0,0 +1,41 @@
+package jm.task.core.jdbc.util;
+
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        StandardServiceRegistry registry = null;
+        try {
+            registry = new StandardServiceRegistryBuilder()
+                    .applySetting("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver")
+                    .applySetting("hibernate.connection.url", "jdbc:mysql://localhost:3306/users_db")
+                    .applySetting("hibernate.connection.username", "root")
+                    .applySetting("hibernate.connection.password", "Purple8star#")
+                    .applySetting("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                    .applySetting("hibernate.hbm2ddl.auto", "none")   // или update/validate/create
+                    .applySetting("hibernate.show_sql", "true")
+                    .applySetting("hibernate.format_sql", "true")
+                    .build();
+
+            MetadataSources sources = new MetadataSources(registry);
+            sources.addAnnotatedClass(jm.task.core.jdbc.model.User.class);
+            sources.addAnnotatedClass(User.class);
+
+            return sources.buildMetadata().buildSessionFactory();
+        } catch (Exception e) {
+            StandardServiceRegistryBuilder.destroy(registry);
+            throw new RuntimeException("Ошибка при создании SessionFactory", e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
